f1fc314e4f694d1b05de92f625923e11ee3fc1ad
SANHOKIM <awrko@naver.com>
2023-12-11 12:43:39 +0900

    리액트첫테스트

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo512.png"></img>
+        소스 호출하기 (public은 루트 디렉토리로 / 하면 됩니다.)
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/Hd.js
@@ -0,0 +1,21 @@
+import '../App.css';
+/* import ReactDom from 'react-dom/client';
+import App from './App';
+import Head from './layout/Hd'
+ */
+function Hd() {
+    return (
+
+
+        <>
+            <img src="/logo512.png"></img>
+            <p> 내가 만든 컴포넌트 임다 </p>
+            <p> 반드시 대문자, 함수내의 return식이 있어야 한다. </p>
+            <p>export</p>
+
+        </>
+
+    );
+}
+
+export default Hd;
